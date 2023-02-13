fd0fa2858e0b174f0413168823f8292d89a48b8d
aowen7 <122053762+aowen7@users.noreply.github.com>
2023-02-13 11:59:45 -0800

    initial commit
    declared

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
